214f6346ab13db587094230e1fd7614468e0a556
Михаил <MKX0506@mail.ru>
2024-01-18 15:49:44 +0300

    меню для ИП

--- magic.py
+++
@@ -1,2 +0,0 @@
-def magicFoo:
-    print("some magic")
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def menu():
+    print("Добро пожаловать в (название проекта)!")
+    print("Для начала введите - START")
+    print("Для выхода введите - EXIT")
+
+while True:
+    menu()
+    vhod = input()
+    if vhod == "START":
+        pass
+    elif vhod == "EXIT":
+        break
